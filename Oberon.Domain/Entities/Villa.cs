7b6ef7d7820f46de9daaf3a68076ea2a7ff86ac4
Breno Santiago De Sousa <dev.santiago@proton.me>
2025-09-24 19:07:38 -0300

    Tornar classe Villa pública e adicionar propriedades
    A classe `Villa` foi alterada de `internal` para `public`,
    permitindo acesso fora do assembly.
    
    Adicionadas novas propriedades à classe `Villa`:
    - `Id`, `Name`, `Description`, `Price`, `Occupancy`,
      `ImageUrl`, `CreatedDate` e `UpdatedDate`.

--- Oberon.Domain/Entities/Villa.cs
+++ Oberon.Domain/Entities/Villa.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Oberon.Domain.Entities
 {
-    internal class Villa
+    public class Villa
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
